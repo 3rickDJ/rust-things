3673966e36ab506cc8ee1f0ea0e1d3af5cc98ae2
3rickDJ <erick@makingdevs.com>
2024-08-25 18:28:13 -0600

    refactor: Update main.rs with lazy loading for images

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,6 @@ fn main() {
 
     // variables
 
-    let my_int: i64= 10;
-    println!("{my_int}");
-    let mut float_number = 10.0;
-    float_number = float_number + my_int as f64;
-    println!("{float_number}");
-
-    let mut my_bool = true;
-    println!("{my_bool}");
-
-    my_bool = false || true;
-    println!("{my_bool}");
-
-    const MY_CONST:&str  = "hola";
-    println!("{MY_CONST}");
-    
+    let mut my_list = vec!["a", "b", "c"];
+    println!("{:?}", my_list);
 }
