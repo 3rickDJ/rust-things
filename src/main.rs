adcdc60c96084e7e087b1a4c26da28a6ad757aaa
3rickDJ <erick@makingdevs.com>
2024-08-25 18:06:08 -0600

    refactor: Update main.rs with explicit type annotation for variable

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,9 @@ fn main() {
     // todo!() macro
 
     // variables
-    let mut cadena: &str = "Hola mundo";
-    println!("{}", cadena);
-    println!("{cadena}");
-    cadena = "hola mundo 222";
-    println!("{}", cadena);
+    let mut mutable_string: String = String::from("Hola mundo");
+    println!("{mutable_string}");
+    mutable_string = String::from("Hola mundo 2");
+    println!("{mutable_string}")
     
 }
