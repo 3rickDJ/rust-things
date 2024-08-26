ba8c9ad890289688aea05dc663a81730cd869dd9
3rickDJ <erick@makingdevs.com>
2024-08-25 18:32:10 -0600

    refactor: Update main.rs to print the last element of the list

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 fn main() {
-    // un comentario
-    /*
-     * comentario multilinea
-     */
-    // todo!() macro
-
-    // variables
 
     let mut my_list = vec!["a", "b", "c"];
     println!("{:?}", my_list);
     my_list.push("d");
     println!("{my_list:?}");
-    println!("{}", my_list[0]);
+    println!("{}", my_list[my_list.len() - 1]);
+
 }
