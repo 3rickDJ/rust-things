01502c4043921a3765a76379eb056318e432a339
3rickDJ <erick@makingdevs.com>
2024-08-25 18:12:33 -0600

    refactor: Update main.rs with explicit type annotation for variable

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ fn main() {
     // variables
     let mut mutable_string: String = String::from("Hola mundo");
     println!("{mutable_string}");
-    mutable_string = String::from("Hola mundo 2");
-    println!("{mutable_string}")
+    mutable_string = String::from("o 2");
+    println!("{mutable_string}");
+
+    let mut my_int: i32 = 7;
+    print!("{my_int}");
+
+    my_int = my_int + 3;
+    print!("{my_int}");
+
     
 }
