d80b65b0a0e975f2836d65647e112add10a3f3f0
3rickDJ <erick@makingdevs.com>
2024-09-28 20:58:35 -0600

    add: baby steps,ratatui hello word

--- baby_steps/ratatui-hello/src/main.rs
+++ baby_steps/ratatui-hello/src/main.rs
@@ -1,33 +1,77 @@
 use std::io;
-use crossterm::event::Event;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind };
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
+    buffer::Buffer,
+    layout::{Alignment, Rect},
     style::Stylize,
-    widgets:: Paragraph,
-    DefaultTerminal
+    symbols::border,
+    text::{Line, Text},
+    widgets::{
+        block::{Position, Title},
+        Block, Paragraph, Widget,
+    },
+    DefaultTerminal, Frame
 };
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
-    terminal.clear()?;
-    let app_result = run(terminal);
+    let app_result = App::default().run(& mut terminal);
     ratatui::restore();
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello ratatui (press 'q' to quit)")
-            .white().on_black();
-
-        frame.render_widget(greeting, frame.area());
-        })?;
-
-        if let Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q'){
-                return Ok(());
-            }
+impl App {
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            // self.handle_events()?;
         }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) ->io::Result<()> {
+        todo!()
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Counter App Tutorial ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
